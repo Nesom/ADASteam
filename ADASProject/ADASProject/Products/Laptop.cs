dc95785c60d9db28f3e3702a253bccfbc6d9c765
DmitriiPodlev <42978023+DmitriiPodlev@users.noreply.github.com>
2019-12-02 14:18:34 +0300

    New_products

---
+++ ADASProject/ADASProject/Products/Laptop.cs
@@ -0,0 +1,55 @@
+﻿using ADASProject.Attributes;
+
+namespace ADASProject.Products
+{
+    [ClassName("Laptop")]
+    [ImageSize(120, 180)]
+    public class Laptop : Product<LaptopDescription>
+    {
+    }
+
+    public class LaptopDescription : IDescription
+    {
+        public LaptopDescription() { }
+
+        public LaptopDescription(string operatingSystem, double screenDiagonal, string line,
+            string memory, bool cardModel, bool hdd, string ssd)
+        {
+            OperatingSystem = operatingSystem;
+            ScreenDiagonal = screenDiagonal;
+            CPULine = line;
+            Memory = memory;
+            CardModel = cardModel;
+            HDD = hdd;
+            SSD = ssd;
+        }
+
+        public int Id { get; set; }
+
+        [Category("Classification")]
+        [Characteristic("Operating system")]
+        public string OperatingSystem { get; set; }
+        
+        [Category("Screen")]
+        [Characteristic("Screen diagonal")]
+        public double ScreenDiagonal { get; set; }
+
+        [Category("CPU")]
+        [Characteristic("CPU line")]
+        public string CPULine { get; set; }
+
+        [Category("RAM")]
+        [Characteristic("RAM size")]
+        public string Memory { get; set; }
+
+        [Category("Graphics accelerator")]
+        [Characteristic("Discrete graphics card model")]
+        public bool CardModel{ get; set; }
+
+        [Category("Data storage")]
+        [Characteristic("Total hard drive space")]
+        public bool HDD { get; set; }
+        [Characteristic("Total solid state drives")]
+        public string SSD { get; set; }
+    }
+}
---
+++ ADASProject/ADASProject/Products/TV.cs
@@ -0,0 +1,46 @@
+﻿using ADASProject.Attributes;
+
+namespace ADASProject.Products
+{
+    [ClassName("TV")]
+    [ImageSize(120, 180)]
+    public class TV : Product<TVDescription>
+    {
+    }
+
+    public class TVDescription : IDescription
+    {
+        public TVDescription() { }
+
+        public TVDescription(double screenDiagonal, string screenResolution, bool operatingSystem,
+            bool supportSmartTV, string digitalTuners)
+        {
+            ScreenDiagonal = screenDiagonal;
+            ScreenResolution = screenResolution;
+            OperatingSystem = operatingSystem;
+            SupportSmartTV = supportSmartTV;
+            DigitalTuners = digitalTuners;
+        }
+
+        public int Id { get; set; }
+
+        [Category("Screen")]
+        [Characteristic("Screen diagonal")]
+        public double ScreenDiagonal { get; set; }
+
+        [Characteristic("Screen resolution")]
+        public string ScreenResolution { get; set; }
+
+        [Category("Smart TV")]
+        [Characteristic("Operating system")]
+        public bool OperatingSystem { get; set; }
+
+        [Characteristic("Support Smart TV")]
+        public bool SupportSmartTV { get; set; }
+
+        [Category("Signal reception")]
+        [Characteristic("Digital tuners")]
+        public string DigitalTuners { get; set; }
+    }
+}
+
